4e00f01a9a38187c6118fd2148195c5e43d3fb9c
rakuda-daraku <rakugaki.oukoku@gmail.com>
2017-01-10 00:33:01 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: './dist/',
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    ]
+  }
+}
